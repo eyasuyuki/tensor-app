1ac40eab9eae1363bf09c66f52a97b2232a8f969
Yasuyuki ENDO <eyasuyuki@gmail.com>
2019-10-29 08:18:45 +0900

    add FirebaseInputOutputOptions

--- lib/model.dart
+++ lib/model.dart
@@ -3,15 +3,18 @@ import 'dart:typed_data';
 import 'package:mlkit/mlkit.dart';
 
 class LcdModel {
+  static const _modelName = "LCDModel";
+  static const _options = FirebaseModelInputOutputOptions(
+      [FirebaseModelIOOption(FirebaseModelDataType.BYTE, [64, 64, 1])],
+      [FirebaseModelIOOption(FirebaseModelDataType.BYTE, [1, 10])]);
   final FirebaseModelInterpreter _interpreter = FirebaseModelInterpreter.instance;
   final FirebaseModelManager _manager = FirebaseModelManager.instance;
-  final _modelName = "LCDModel";
   LcdModel() {
     _manager.registerLocalModelSource(
         FirebaseLocalModelSource(modelName: _modelName, assetFilePath: "assets/train.tflite"));
   }
 
   Future<List> run(Uint8List inputBytes) async {
-    return _interpreter.run(localModelName: _modelName, inputBytes: inputBytes);
+    return _interpreter.run(localModelName: _modelName, inputOutputOptions: _options, inputBytes: inputBytes);
   }
 }
